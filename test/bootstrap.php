dc7346066ac16dc0936a4bd0c53b39cc452a4972
David Naber <kontakt@dnaber.de>
2015-01-27 17:32:11 +0100

    add wp_mock as dev-dependency

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -6,6 +6,7 @@ use Requisite;
 
 require_once dirname( __DIR__ ) . '/inc/init-requisite.php';
 require_once dirname( __DIR__ ) . '/inc/register-autoloding.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 $requisite = WPAPIAdapter\init_requisite( dirname( __DIR__ ) . '/lib' );
 WPAPIAdapter\register_autoloading( dirname( __DIR__ ), $requisite );
