734eb0fb6a642b46ddb28cce8e226895a0b00a39
David Naber <kontakt@dnaber.de>
2015-01-28 17:19:32 +0100

    test iterator with multiple handlers

--- test/Unit/EntityIteratorTest.php
+++ test/Unit/EntityIteratorTest.php
@@ -98,7 +98,78 @@ class EntityIteratorTest extends TestCase\MockCollectionTestCase {
 		}
 	}
 
+	/**
+	 * test a combination of more than one handler
+	 */
+	public function test_rename_and_remove_fields() {
 
+		$entity = $this->get_test_entiy();
+		$entity_array = get_object_vars( $entity );
+		$keys = array_keys( $entity_array );
+
+		// to unset a field, the handler has to return an empty string on get_name()
+		// we want to rename author to author_ID
+		$unset_status_mock = $this->get_rename_field_handler_mock();
+		$unset_status_mock->expects( $this->any() )
+			->method( 'get_name' )
+			->willReturn( '' );
+
+		// restructure the field "author" (rename it to author_ID and make the value an integer)
+		$update_author_mock = $this->get_rename_field_handler_mock();
+		$update_author_mock->expects( $this->any() )
+			->method( 'get_name' )
+			->willReturn( 'author_ID' );
+		$update_author_mock->expects( $this->any() )
+			->method( 'get_value' )
+			->willReturn( 12 );
+
+		$field_handlers = array(
+			'status' => array( $unset_status_mock ),
+			'author' => array( $update_author_mock )
+		);
+		$repo_mock = $this->get_field_handler_repository_mock( $field_handlers );
+
+		$testee = new WPAPIAdapter\Iterator\EntityIterator( $entity, $repo_mock );
+
+		// the iteration
+		while( $testee->valid() ) {
+			$testee->process_field();
+			$testee->next();
+		};
+
+		// check existence of any key except 'status' and 'author'
+		foreach ( $keys as $key ) {
+			switch ( $key ) {
+				case 'status' :
+				case 'author' :
+					$this->assertObjectNotHasAttribute(
+						$key,
+						$entity
+					);
+					break;
+				default :
+					$this->assertObjectHasAttribute(
+						$key,
+						$entity
+					);
+					// check data consistency
+					$this->assertEquals(
+						$entity_array[ $key ],
+						$entity->{ $key }
+					);
+					break;
+			}
+		}
+		// test the renamed key
+		$this->assertObjectHasAttribute(
+			'author_ID',
+			$entity
+		);
+		$this->assertSame(
+			12,
+			$entity->author_ID
+		);
+	}
 
 	/**
 	 * @return object
@@ -109,7 +180,11 @@ class EntityIteratorTest extends TestCase\MockCollectionTestCase {
 			'ID'      => 42,
 			'title'   => 'My test object',
 			'content' => 'Some content in here',
-			'author'  => 12
+			'status'  => 'publish',
+			'author'  => array(
+				'ID' => 12,
+				'name' => 'John'
+			)
 		);
 	}
 }
