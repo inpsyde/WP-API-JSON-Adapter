73d927551f66aa6a183b45655fa45af46d76f17d
David Naber <kontakt@dnaber.de>
2015-01-29 19:12:33 +0100

    plugin lift-off

--- wp-api-json-adapter.php
+++ wp-api-json-adapter.php
@@ -11,3 +11,12 @@
 
 namespace WPAPIAdapter;
 
+$file_loader = function( $file ) {
+
+	require_once __DIR__ . '/inc/' . $file;
+};
+$file_loader( 'init-requisite.php' );
+$file_loader( 'register-autoloading.php' );
+$file_loader( 'init.php' );
+
+add_action( 'wp_loaded', __NAMESPACE__ . '\init' );
