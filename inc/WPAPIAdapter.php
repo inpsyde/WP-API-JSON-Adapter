a2530b444e30cd76b53855fc9cdad77d2eee4ca9
David Naber <kontakt@dnaber.de>
2015-01-29 19:11:55 +0100

    introduce WPAPIAdapter\WPAPIAdapter

---
+++ inc/WPAPIAdapter.php
@@ -0,0 +1,38 @@
+<?php # -*- coding: utf-8 -*-
+
+
+namespace WPAPIAdapter;
+
+class WPAPIAdapter {
+
+	/**
+	 * @type Builder\FieldsControllerBuilder
+	 */
+	private $fields_ctrl_builder;
+
+	/**
+	 * @type Builder\EndpointParserBuilder
+	 */
+	private $endpoint_parser_builder;
+
+	public function __construct() {
+
+		$this->fields_ctrl_builder = new Builder\FieldsControllerBuilder;
+		$this->endpoint_parser_builder = new Builder\EndpointParserBuilder;
+	}
+
+	/**
+	 * build the DataShaper and place the filter callback
+	 */
+	public function run() {
+
+		$data_shaper = new Core\DataShaper( $this->endpoint_parser_builder );
+		$data_shaper->add_entity_controller(
+			'posts',
+			$this->fields_ctrl_builder->build_post_fields_controller()
+		);
+		// Todo: more entities up to come
+
+		add_filter( 'json_pre_dispatch', array( $data_shaper, 'shape_data' ), 10, 2 );
+	}
+} 
