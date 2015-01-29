a53d1c918f09072bca19ab9d482f0551aeec91a7
David Naber <kontakt@dnaber.de>
2015-01-29 19:11:18 +0100

    introduce Builder\FieldsControllerBuilder

---
+++ inc/Builder/FieldsControllerBuilder.php
@@ -0,0 +1,32 @@
+<?php # -*- coding: utf-8 -*-
+
+
+namespace WPAPIAdapter\Builder;
+use WPAPIAdapter\Core;
+use WPAPIAdapter\FieldHandlerRepository;
+
+class FieldsControllerBuilder {
+
+	public function build_post_fields_controller() {
+
+		$repos = $this->get_new_repositorys();
+
+		$controller = new Core\PostFieldsController(
+			$repos[ 'change' ],
+			$repos[ 'add' ]
+		);
+
+		return $controller;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function get_new_repositorys() {
+
+		return array(
+			'add'    => new FieldHandlerRepository,
+			'change' => new FieldHandlerRepository
+		);
+	}
+} 
